4652b6286c72b4b4ea10f506acd8bb1a7772efa5
ochir <ochir.b@tanasoft.mn>
2025-02-21 11:55:47 +0800

    setup

--- lua/community.lua
+++ lua/community.lua
@@ -7,5 +7,7 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.java" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
   -- import/override with your plugins folder
 }
